e396b6fdf462db26ebc5c61be9dfa49e40fbceba
UmerAli169 <umer@idenbrid.com>
2025-02-24 09:52:09 +0500

    good to go

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,20 +1,11 @@
-"use client"
-import { useState } from "react";
-import { ResetPasswordModal } from "../components/auth/modals/ResetPasswordModal"; // Adjust path as needed
-import { AuthButton } from "../components/auth/common/AuthButton";
+import React from 'react'
 
-export default function Home() {
-  const [isResetModalOpen, setIsResetModalOpen] = useState(false);
-
+function page() {
   return (
-    <div className="flex justify-center items-center min-h-screen">
-      <AuthButton onClick={() => setIsResetModalOpen(true)}>Open Reset Password Modal</AuthButton>
-
-      <ResetPasswordModal 
-        isOpen={isResetModalOpen} 
-        onClose={() => setIsResetModalOpen(false)} 
-        token="test-token" 
-      />
+    <div>
+      HomePage
     </div>
-  );
+  )
 }
+
+export default page
