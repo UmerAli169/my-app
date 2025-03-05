71ee42ff0eb27376596df9982707ec0a785c6ba3
UmerAli169 <umer@idenbrid.com>
2025-03-05 13:58:06 +0500

    s

---
+++ src/app/(screens)/Account/changepassword/page.tsx
@@ -0,0 +1,13 @@
+"use client"
+import AccountLayout from "../../../../components/account/AccountLayout";
+import ChangePasswordForm from '../../../../components/account/ChangePasswordForm'
+
+const ContactInformation = () => {
+  return (
+    <AccountLayout>
+      <ChangePasswordForm />
+    </AccountLayout>
+  );
+};
+
+export default ContactInformation;
